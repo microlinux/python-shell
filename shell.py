9d6426c268916a2d96098a7527694963a5b9cccb
microlinux <toddmueller@gmail.com>
2013-03-31 22:45:10 -0500

    complete re-write

--- shell.py
+++ shell.py
@@ -1,121 +1,197 @@
 from collections import namedtuple
-from multiprocessing import Process, Queue
+from itertools import imap
+from multiprocessing import Pool
 from shlex import split
 from subprocess import PIPE, Popen, STDOUT
-from threading import Timer
-from time import sleep
+
+""" Strips whitespace from command output.
+
+Strips horizontal and vertical whitespace from command output and returns a
+corresponding list.
+
+@param  <list>  command output
+@return <list>  stripped command output
 
 """
-@desc strip leading/trailing blank lines and horizontal whitespace from a list
-      of lines, helpful for mechanized processing
+def strip_output(output):
+  for i in range(0, len(output)):
+    output[i] = output[i].strip()
 
-@param  <list>  lines
+  while output and not output[-1]:
+    output.pop()
 
-@return <list>  formatted list
-"""
-def format_output(output):
-  num_lines = len(output)
-  begin = 0
-  end = 0
-
-  if num_lines > 0:
-    # find index of first non-blank line
-    for i in range(0, num_lines):
-      if output[i].strip():
-        begin = i
-        break
-
-    # find index of last non-blank line
-    for i in range(num_lines - 1, -1, -1):
-      if output[i].strip():
-        end = i + 1
-        break
-
-    # slice off leading/trailing blank lines
-    output = output[begin:end];
-
-    # strip horizontal whitespace
-    for i in range(0, len(output)):
-      output[i] = output[i].strip()
+  while output and not output[0]:
+    output.pop(0)
 
   return output
 
-"""
-@desc dual purpose command forker; if given a queue, place result into it as a
-      list, else return a namedtuple (see below, list follows same order)
+""" Parses a command result.
 
-      command <str>   command to execute
-      tokens  <list>  tokenized command
-      retval  <int>   command retval (-15 on timeout)
-      output  <list>  output parsed by format_output() (empty on timeout)
+Parses a command result and returns a corresponding namedtuple.
 
-@param  <str>           command to execute
-@param  <int>           timeout in seconds (default: 30)
-@param  Process.Queue   optional queue to place result in
-@param  <int>           required only if a queue is given, index in list of
-                        commands given to multi_command
-
-@return NamedTuple|None result or None, depending if a queue was given
-"""
-def command(command, timeout=30, queue=None, index=None):
-  tokens = split(command)
-
-  # fork command
-  proc = Popen(tokens, stdout=PIPE, stderr=STDOUT, close_fds=True)
-
-  # setup and start timer
-  kill = lambda p: p.terminate()
-  timer = Timer(timeout, kill, [proc])
-  timer.start()
-
-  # get output
-  output = proc.communicate()[0].split('\n')
-
-  # cancel timer, command finished before timeout
-  timer.cancel()
-
-  output = format_output(output)
-
-  # determine out how function is being used and take appropriate action with
-  # the result
-  if queue:
-    queue.put([index, [command, tokens, proc.returncode, output]])
-    return None
-  else:
-    return namedtuple("ResultTuple", ['command', 'tokens', 'retval', 'output'])._make([command, tokens, proc.returncode, output])
+@param  <list>        command result
+@return <namedtuple>  command result
 
 """
-@desc forks mutilple shell commands concurrently and returns a list of results
-      as namedtuples in the same order as the commands were given
+def parse_result(result):
+  return namedtuple("ResultTuple", ['command', 'retval', 'output'])._make(
+                    [result[0], result[1], result[2]])
 
-@param  <list>  commands to execute
-@param  <int>   individual command timeout in seconds (default: 30)
+""" Multiprocessing Pool worker.
 
-@return <list> command results
+Runs a command passed as a list of arguments. Requires /usr/bin/tmeout.
+Returns the command result as a list.
+
+@param  <list>  command string, timeout in seconds
+@return <list>  command string, reval, stripped output
+
 """
-def multi_command(commands, timeout=30):
-  num_cmds = len(commands)
-  queue = Queue()
-  procs = []
+def worker(job):
+  retval = None
+  output = []
 
-  # pre-create result list indexes since we may need to access them out-of-order
-  # when populating it
-  results = [None] * num_cmds
+  try:
+    """ Fork the command, time out after x seconds. """
+    proc = Popen(split('/usr/bin/timeout %s %s' % (job[1], str(job[0]))),
+                 stdout=PIPE, stderr=STDOUT, close_fds=True)
+    proc.wait()
+  except OSError:
+    """ Special case, /usr/bin/timeout not found. """
+    retval = 255
+    output = ['/usr/bin/timeout not found']
+  except Exception, e:
+    """ Special case, unexpected exception. """
+    retval = 256
+    output = ['%s' % strip_output(e.split('\n'))]
+  finally:
+    if retval != 255 and retval != 256:
+      if proc.returncode == 124:
+        """ Special case, command timed out. """
+        retval = 124;
+        output = ['command timed out']
+      elif proc.returncode == 127:
+        """ Special case, command not found or executable. """
+        retval = 127
+        output = ['command not found or not executable']
+      else:
+        """ Nothing unexpected happened, process result normally. """
+        retval = proc.returncode
+        try:
+          output = strip_output(proc.communicate()[0].split('\n'))
+        except Exception, e:
+          """ Special case, unexpected exception. """
+          retval = 257
+          output = ['%s' % strip_output(e.split('\n'))]
+          pass
 
-  # fork and start commands
-  for i in range(0, num_cmds):
-    # since commands can complete out of order, pass the index of the command
-    # from the given list so we can re-order results to match
-    procs.append(Process(target=command, args=(commands[i], timeout, queue, i)))
-    procs[i].start()
+    return [job[0], retval, output]
 
-  # get results
-  for i in range(0, num_cmds):
-    result = queue.get()
-    results[int(result[0])] = namedtuple("ResultTuple", ['command', 'tokens', 'retval', 'output'])._make([result[1][0], result[1][1], result[1][2],result[1][3]])
+""" Execute a single command.
 
-  # signal subprocesses complete
-  for i in range(0, num_cmds):
-    procs[i].join();
+Runs a command and returns the result as a namedtuple.
 
-  return results
+@param  <str> command string
+@param  <int> timeout in seconds
+@return <namedtuple>  command string, retval, stripped output
+
+"""
+def command(command, timeout=10):
+  assert type(command) is str, 'command is not a string'
+  assert type(timeout) is int, 'timeout is not an integer'
+
+  """ Run the command. """
+  result = worker([command, timeout])
+
+  return parse_result(result)
+
+""" Executes multiple commands concurrently.
+
+Creates a pool of the given size and runs multiple commands concurrently.
+Returns a generator that returns ordered namedtuples of results.
+
+@param  <list>  command strings
+@param  <int>   individual command timeout in seconds
+@param  <int>   number of workers in pool
+@return <generator> ordered namedtuples of results
+
+"""
+def multi_command(commands, timeout=10, workers=4):
+  assert type(commands) is list, 'commands is not a list'
+  for i in range(len(commands)):
+    assert type(commands[i]) is str, 'commands[%s] is not a string' % i
+  assert type(timeout) is int, 'timeout is not an integer'
+  assert type(workers) is int, 'workers is not an integer'
+
+  count = len(commands)
+  result_tuples = []
+
+  """ No sense in having more workers than jobs. """
+  if workers > count:
+    workers = count
+
+  """
+  Pool.imap can only pass one argument to a worker. To get around this, we
+  put multiple arguments into a list.
+  """
+  for i in range(count):
+    commands[i] = [commands[i], timeout]
+
+  """ Fire up the pool, collect ordered results. """
+  pool = Pool(processes=workers)
+  results = pool.imap(worker, commands)
+
+  """ Wait for all jobs to finish """
+  pool.close()
+  pool.join()
+
+  return imap(parse_result, results)
+
+""" Example usage. """
+if __name__ == '__main__':
+  i = 0
+  print '---------------------'
+  print 'Execute one command'
+  print '---------------------'
+  result = command('whoami', 2)
+  print 'result.command: %s' % result.command
+  print 'result.retval : %s' % result.retval
+  print 'result.output : %s' % result.output
+  print
+  print '----------------------'
+  print 'Execute two commands'
+  print '----------------------'
+  results = multi_command(['whoami', 'ping -c3 -i1 -W1 -q google.com'], 5)
+  for result in results:
+    print 'result%s.command: %s' % (i, result.command)
+    print 'result%s.retval : %s' % (i, result.retval)
+    print 'result%s.output : %s' % (i, result.output)
+    print
+    i = i + 1
+  print '----------------------------------'
+  print 'Execute one non-existent command'
+  print '----------------------------------'
+  result = command('/usr/bin/fake_binary_99 -f fake_opt', 2)
+  print 'result.command: %s' % result.command
+  print 'result.retval : %s' % result.retval
+  print 'result.output : %s' % result.output
+  print
+  print '------------------------------------'
+  print 'Execute one command that times out'
+  print '------------------------------------'
+  result = command('ping -c3 -i1 -W1 -q google.com', 2)
+  print 'result.command: %s' % result.command
+  print 'result.retval : %s' % result.retval
+  print 'result.output : %s' % result.output
+  print
+  print '---------------------------------------------'
+  print 'Execute three commands with varying results'
+  print '---------------------------------------------'
+  results = multi_command(['ls -l', '/usr/bin/fake_binary_99 -f fake_opt',
+                           'ping -c3 -i1 -W1 -q google.com'], 2)
+  i = 0
+  for result in results:
+    print 'result%s.command: %s' % (i, result.command)
+    print 'result%s.retval : %s' % (i, result.retval)
+    print 'result%s.output : %s' % (i, result.output)
+    print
+    i = i + 1
