8e5c195fa3d5794d8899cd08ab4b839036c54ed0
microlinux <toddmueller@gmail.com>
2013-04-01 20:59:18 -0500

    update compressed version, add version to files

--- compressed/shell.py
+++ compressed/shell.py
@@ -1,31 +1,34 @@
+""" Version 1.0-20130401 """
 from collections import namedtuple
 from itertools import imap
 from multiprocessing import Pool
 from shlex import split
 from subprocess import PIPE, Popen, STDOUT
+from traceback import format_exc
 def strip_output(output):
   for i in range(0, len(output)):
-   output[i] = output[i].strip()
+    output[i] = output[i].strip()
   while output and not output[-1]:
     output.pop()
   while output and not output[0]:
     output.pop(0)
   return output
 def parse_result(result):
-  return namedtuple("ResultTuple", ['command', 'retval', 'output'])._make([result[0], result[1], result[2]])
+  return namedtuple("ResultTuple", ['command', 'retval', 'output'])._make(
+                    [result[0], result[1], result[2]])
 def worker(job):
   output = []
   retval = None
   try:
-    proc = Popen(split('/usr/bin/timeout %s %s' % (job[1], str(job[0]))), stdout=PIPE, stderr=STDOUT, close_fds=True)
+    proc = Popen(split('/usr/bin/timeout %s %s' % (job[1], str(job[0]))),
+                 stdout=PIPE, stderr=STDOUT, close_fds=True)
     proc.wait()
   except OSError:
     retval = 255
     output = ['/usr/bin/timeout not found or not executable']
   except Exception, e:
     retval = 256
-    output = '%s' % e
-    output = strip_output(output.split('\n'))
+    output = format_exc().splitlines()
   finally:
     if retval != 255 and retval != 256:
       if proc.returncode == 124:
@@ -37,18 +40,16 @@ def worker(job):
       else:
         retval = proc.returncode
         try:
-          output = strip_output(proc.communicate()[0].split('\n'))
+          output = proc.communicate()[0].splitlines()
         except Exception, e:
           retval = 257
-          output = '%s' % e
-          output = strip_output(output.split('\n'))
+          output = format_exc().splitlines()
           pass
-    return [job[0], retval, output]
+    return [job[0], retval, strip_output(output)]
 def command(command, timeout=10):
   assert type(command) is str, 'command is not a string'
   assert type(timeout) is int, 'timeout is not an integer'
-  result = worker([command, timeout])
-  return parse_result(result)
+  return parse_result(worker([command, timeout]))
 def multi_command(commands, timeout=10, workers=4):
   assert type(commands) is list, 'commands is not a list'
   for i in range(len(commands)):
@@ -64,4 +65,4 @@ def multi_command(commands, timeout=10, workers=4):
   results = pool.imap(worker, commands)
   pool.close()
   pool.join()
-  return imap(parse_result, results
+  return imap(parse_result, results)
