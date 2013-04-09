6ee51c4a7c2be6ad9db718eb798e7330bb05d9ce
microlinux <toddmueller@gmail.com>
2013-04-08 19:30:39 -0500

    doc updates

--- shell.py
+++ shell.py
@@ -10,7 +10,7 @@ See https://github.com/microlinux/python-shell/blob/master/README.
 
 This program is free software: you can redistribute it and/or modify it
 under the terms of the GNU General Public License as published by the Free
-Software Foundation, version 3.
+Software Foundation, version 3. See https://www.gnu.org/licenses/.
 
 """
 from collections import namedtuple
